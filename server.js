06755a993d7960f3b07887cea7c68fd0a980309c
Niels Koster <n.koster@portavita.eu>
2021-04-18 19:44:33 +0200

    Use highest quality

--- server.js
+++ server.js
@@ -1,35 +1,59 @@
 (_ => {
 
   const express = require('express')
-  const app = express()
   const cors = require('cors')
-  const apiPort = 3567
   const ytdl = require('ytdl-core')
+
+  const app = express()
+
+  const apiPort = 3567
 
   app.use(cors())
   app.use(express.json())
 
   const api = async (req, res) => {
-    console.log(req.params.link)
+
     const videoUrl = 'https://www.youtube.com/watch?v=' + req.params.link
+    
     let vid
+    
+    res.setHeader('Content-Type', 'application/json')
+
     try {
       vid = ytdl.getVideoID(videoUrl)
     } catch (err) {
       console.log(err.message)
-      process.exit(1)
+      res.send(JSON.stringify({}))
+      return
     }
+    
     const info = await ytdl.getInfo(vid)
-    // console.log(info.formats[info.formats.length - 1].url)
-    res.setHeader('Content-Type', 'application/json')
-    const formats = info.player_response.streamingData.formats
-    // const formats = info.formats
-    console.log(formats[formats.length - 1].url)
-    if (formats) {
-      res.send(JSON.stringify({ 
-        src: formats[formats.length - 1].url
-      }))
+
+    if (info.formats[0].isLive) {
+      console.log('live video')
     }
+
+    info.formats
+    .filter(v => v.width ? false : true)
+    .map(v => {
+      console.log(parseInt(v.qualityLabel))
+    })
+
+    const highestQuality = info.formats
+      .map(f => f.width ? f.width : 0)
+      .reduce((a, b) => a > b ? a : b)
+
+    const indexOfHighestQuality = info.formats
+      .map(f => f.width ? f.width : 0)
+      .indexOf(highestQuality)
+
+    console.log(`video: ${req.params.link}, quality: ${highestQuality}`)
+
+    res.send(JSON.stringify({ 
+      // src: info.formats[indexOfHighestQuality].url
+      info
+    }))
+
   }
   
   app.get('/:link', api)
